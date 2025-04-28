f7aa06323e5d6c0b16f23bc6cfb0f49b6bc0cd1c
Cristian Barac Fabregat Gallegos <barac.fabregat.gallegos@tuta.com>
2025-04-28 06:35:33 -0600

    fix: change driverId column name

--- src/travel/schema.ts
+++ src/travel/schema.ts
@@ -2,12 +2,12 @@ import { integer, real, sqliteTable } from 'drizzle-orm/sqlite-core';
 import { users } from 'src/users/schema';
 
 export const travels = sqliteTable('travels', {
-  id: integer('id').primaryKey(),
-  price: real('price').notNull(),
-  clientId: integer('client_id')
-    .notNull()
-    .references(() => users.id, { onDelete: 'cascade' }),
-  driverId: integer('client_id')
-    .notNull()
-    .references(() => users.id, { onDelete: 'cascade' }),
+	id: integer('id').primaryKey(),
+	price: real('price').notNull(),
+	clientId: integer('client_id')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	driverId: integer('driver_id')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
 });
